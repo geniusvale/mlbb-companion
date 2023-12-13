bf5eb3b716ad6a6f49d28fe94af8cf9769f7838d
geniusvale <ariiqromero@gmail.com>
2023-12-14 00:44:37 +0700

    add popular build with tooltip

--- src/components/heroStatsModal.jsx
+++ src/components/heroStatsModal.jsx
@@ -18,6 +18,7 @@ import {
   Box,
   Flex,
   Spacer,
+  Tooltip,
 } from "@chakra-ui/react";
 import LoremIpsum from "react-lorem-ipsum";
 import api from "../api";
@@ -27,14 +28,16 @@ function HeroStatsModal({ heroName, heroId }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [heroData, setHeroData] = useState({});
   const [skills, setSkills] = useState([]);
+  const [items, setItems] = useState([]);
 
   async function getHeroDetail() {
     onOpen();
     try {
       const response = await api.get(`/heroes/${heroId}`);
-      console.log(response.data["data"]["skill"]);
       setHeroData(response.data["data"]);
       setSkills(response.data["data"]["skill"]["skill"]);
+      setItems(response.data["data"]["gear"]["out_pack"]);
+      console.log(response.data["data"]["gear"]["out_pack"]["equip"]);
     } catch (error) {
       console.log(error);
     }
@@ -58,12 +61,20 @@ function HeroStatsModal({ heroName, heroId }) {
           <ModalHeader>{heroName}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Heading as='h5' size='md' marginBottom="32px">{heroData.type}</Heading>
+            <Heading as="h5" size="md" marginBottom="8px">
+              Role/Lane
+            </Heading>
+
+            <Text marginBottom="32px">{heroData.type}</Text>
+
+            <Heading as="h5" size="md" marginBottom="32px">
+              Skills
+            </Heading>
 
             {skills.map((data, index) => {
               return (
-                <Box key={index}>
-                  <Flex gap="4" marginBottom="16px">
+                <Box key={index} marginBottom="32px">
+                  <Flex gap="4">
                     <Image
                       src={data.icon}
                       alt="Hero Skill"
@@ -82,6 +93,30 @@ function HeroStatsModal({ heroName, heroId }) {
               );
             })}
 
+            <Heading as="h5" size="md" marginBottom="32px">
+              Popular Build
+            </Heading>
+
+            <Box>
+              <Flex justifyContent='center'>
+                {items.map((data, index) => {
+                  return (
+                    <Tooltip label={data.equip.des} key={index} placement="top">
+                      <Container centerContent>
+                        <Image
+                          src={data.equip.icon}
+                          alt="Item"
+                          fallbackSrc="https://via.placeholder.com/64"
+                          boxSize="48px"
+                          borderRadius="full"
+                        />
+                        <Text align='center'>{data.equip.name}</Text>
+                      </Container>
+                    </Tooltip>
+                  );
+                })}
+              </Flex>
+            </Box>
           </ModalBody>
           <ModalFooter>
             <Button onClick={onClose}>Close</Button>
